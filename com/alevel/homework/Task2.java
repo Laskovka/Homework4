61922344a30c24299a62bc958aa0377a743b9d9d
Laskovka <laskovskiu2017@gmail.com>
2020-12-14 16:06:17 +0200

    second commit

--- com/alevel/homework/Task2.java
+++ com/alevel/homework/Task2.java
@@ -6,17 +6,19 @@ public class Task2 {
     public static void main(String[] args) {
         System.out.println("Enter any sequence of numbers up to 10 in size.");
         Scanner scanner = new Scanner(System.in);
-        double[] sequence = new double[10];
-        for (int i = 0; i < 10; i++) {
+        double[] sequence = new double[3];
+        for (int i = 0; i < 3; i++) {
             System.out.print("Enter " + (i+1) + " number:");
             sequence[i] = scanner.nextDouble();
         }
         System.out.println("Enter \"max\" if you want to find the largest number, or \"min\" if you want to find the smallest number.");
+        String scannerString = "";
         while (true) {
-            if(scanner.nextLine().equals("max")) {
+            scannerString = scanner.nextLine();
+            if(scannerString.equals("max")) {
                 System.out.println("Maximum number is " + getMax(sequence));
                 break;
-            } else if(scanner.nextLine().equals("min")) {
+            } else if(scannerString.equals("min")) {
                 System.out.println("Minimum number is " + getMin(sequence));
                 break;
             }
